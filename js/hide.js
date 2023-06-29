df86fdb201aa2451be1800ec6f8dfcf4b2e6854c
jiseub33 <jiseub33@gmail.com>
2023-06-30 06:46:02 +1100

    update

--- js/hide.js
+++ js/hide.js
@@ -1,25 +1,11 @@
-// Make an AJAX request to fetch the server time
-fetch('/api/server-time')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    var serverTime = new Date(data.serverTime);
-    updateElements(serverTime);
-  })
-  .catch(function(error) {
-    console.error('Error fetching server time:', error);
-  });
+// Get all the elements with the "element" class
+var elements = document.querySelectorAll(".available");
 
-// Function to update elements based on the server time
-function updateElements(serverTime) {
-  var elements = document.querySelectorAll(".available");
-
-  elements.forEach(function(element) {
-    var hideDate = new Date(element.dataset.hideDate);
-    if (serverTime >= hideDate) {
-      element.classList.remove("available");
-      element.classList.add("unavailable");
-    }
-  });
-}
+// Loop over the elements and check the hide date
+elements.forEach(function(element) {
+  var hideDate = new Date(element.dataset.hideDate);
+  if (date >= hideDate) {
+    element.classList.remove("available");
+    element.classList.add("unavailable");
+  }
+});
