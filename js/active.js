0996113b68e6f36efb7c6227b56a4dcffc5ecdd5
jiseub33 <jiseub33@gmail.com>
2023-06-30 06:34:49 +1100

    update

--- js/active.js
+++ js/active.js
@@ -1,22 +1,24 @@
 // Function to compare clipboard text with button text and add/remove class
 function compareTextAndUpdateClass() {
-    navigator.clipboard.readText().then(function(clipboardText) {
-      var buttons = document.getElementsByTagName('button');
+  navigator.clipboard.readText().then(function(clipboardText) {
+    var buttons = document.getElementsByTagName('button');
 
-      for (var i = 0; i < buttons.length; i++) {
-        var buttonText = buttons[i].textContent;
+    for (var i = 0; i < buttons.length; i++) {
+      var buttonText = buttons[i].textContent;
 
-        if (buttonText === clipboardText) {
-          buttons[i].classList.add('active');
-        } else {
-          buttons[i].classList.remove('active');
-        }
+      if (buttonText === clipboardText) {
+        buttons[i].classList.add('active');
+      } else {
+        buttons[i].classList.remove('active');
       }
-    });
-  }
+    }
+  }).catch(function(error) {
+    console.log('Error reading clipboard text:', error);
+  });
+}
 
-  // Call the function initially
-  compareTextAndUpdateClass();
+// Call the function initially
+compareTextAndUpdateClass();
 
-  // Monitor clipboard changes periodically
-  setInterval(compareTextAndUpdateClass, 1); // Change interval as desired
+// Monitor clipboard changes periodically
+setInterval(compareTextAndUpdateClass, 100); // Change interval as desired
