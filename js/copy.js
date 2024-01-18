acb07a6bc3cb14b21b6f8ce69937e56629240521
jiseub33 <jiseub33@gmail.com>
2024-01-19 04:28:34 +0900

    update us

---
+++ js/copy.js
@@ -0,0 +1,10 @@
+var btns = document.querySelectorAll('button');
+var clipboard = new ClipboardJS(btns);
+
+clipboard.on('success', function(e) {
+  console.log(e);
+});
+
+clipboard.on('error', function(e) {
+  console.log(e);
+});
